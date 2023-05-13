f45599123c90fb208061c6d586d32c7d3b3a0ed0
varadpundlik <varadpundlik@gmail.com>
2023-05-13 21:44:43 +0530

    added emailjs

---
+++ src/components/email.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export default function Email(){
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_665bv0c', 'template_by3jb2k', form.current, 'YLxNvOewmXsjtOdEn')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
