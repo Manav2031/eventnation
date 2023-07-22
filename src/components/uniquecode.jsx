452c024443015c14791b0b7c9cf5fb2e4e123443
varadpundlik <varadpundlik@gmail.com>
2023-07-22 19:38:14 +0530

    started backend

--- src/components/uniquecode.jsx
+++ src/components/uniquecode.jsx
@@ -3,16 +3,21 @@ import QRCode from "qrcode.react";
 import { v4 as uuidv4 } from "uuid";
 
 function Ticket(props) {
-  const { name, ticketType } = props;
+  const { name, ticketType, email, frequency, totalPrice } = props;
   const ticketKey = uuidv4();
 
+
   return (
-    <div>
-      <h3>{name}</h3>
+    <div id = "ticket-container">
+      <h3>Name: {name}</h3>
+      <p>Email: {email} </p>
       <p>Ticket Type: {ticketType}</p>
+      <p>Number of Tickets: {frequency}</p>
+      <p>Total Price: {totalPrice}</p>
       <QRCode value={ticketKey} />
     </div>
   );
 }
+
 
 export default Ticket;
